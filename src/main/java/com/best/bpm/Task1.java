0b9231e63c2dc58284f28bc3f443344668b34b46
ben hamida <ben.hamida.souhaiel@gmail.com>
2018-12-20 22:09:46 +0100

    add first task

--- src/main/java/com/best/bpm/Task1.java
+++ src/main/java/com/best/bpm/Task1.java
@@ -3,7 +3,7 @@ package com.best.bpm;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class Test1 implements JavaDelegate {
+public class Task1 implements JavaDelegate {
     public void execute(DelegateExecution delegateExecution) throws Exception {
         System.out.println("test1");
     }
